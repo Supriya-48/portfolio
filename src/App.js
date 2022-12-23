547dafe6e65b158889d826c2d43268152805977f
supriya_muppu <muppusupriya@gmail.com>
2022-12-23 14:31:49 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
+
+import {Route,Routes} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </>
   );
 }
 export default App;
