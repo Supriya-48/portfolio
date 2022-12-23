d2f034fb0907c4a2b9ef848f6ce440882994b85b
Supriya Muppu <supriya.mupputhoughtworks.com@Supriyas-MacBook-Pro.local>
2022-12-23 16:19:18 +0530

    Created title

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,11 +1,20 @@
 import "./HeroImgStyles.css";
 import React from 'react'
-
+import IntroImg from '../assets/IntroImg.jpeg'
+import {Link} from 'react-router-dom'
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            {/* <img className="intro-img" src={} alt={IntroImage}/> */}
+            <img className="intro-img" src={IntroImg} alt="IntroImage"/>
+        </div>
+        <div className="content">
+            <p>
+                HI, I am Supriya Muppu
+            </p>
+            <h1>Full stack Developer</h1>
+            <Link to="/project" className="btn">PROJECTS</Link>
+            <Link to="/contact" className="btn btn-light">CONTACT</Link>
         </div>
     </div>
     )
